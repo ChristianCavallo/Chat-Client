a6cef7c9a2f0bfc789fcfdd683707f7eb4bc7f7f
Noemi Buggea <noemibuggea@live.it>
2019-12-16 18:01:23 +0100

    hjhj

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import sys
+
+from PyQt5.uic.properties import QtWidgets
+
+import Login
+import Registration
 import client
 import json
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from Login import Ui_MainWindow
 
 
-SocketClient = client.SocketClient(('localhost', 15000))
+#SocketClient = client.SocketClient(('localhost', 15000))
+login = Login
+app = login.QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
+print("bella minchiata")
 
-SocketClient.sendMessage("ciao")
+#SocketClient.sendMessage("ciao")
 
 
